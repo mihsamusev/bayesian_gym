8ee10c97e702424a1f46a375abc25877a7485df0
Mihhail <mih.samusev@gmail.com>
2020-06-15 17:42:27 +0200

    started SOT algorithms

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # bayesian_gym
-Collection of courses, examples, notebooks to train many important modern applications of Bayesian filtering such as state estimation, sensor fusion, linear and nonlinear filtering, single and multi-object tracking and more
+Collection of courses, examples, notebooks to train many important modern applications of Bayesian filtering such as state estimation, sensor fusion, linear and nonlinear filtering, single and multi-object tracking and more.
 
 # Sources
+The following precisely describes the sequence of courses and lectures i followed. In the end attempt to souggest an ideal path that weighs all pros and cons of individual courses and gives the most optimal gains for dedicated time.
+
 1) edX course - [Sensor Fusion and Non-linear Filtering for Automotive Systems](https://courses.edx.org/courses/course-v1:ChalmersX+ChM015x+2T2020/course/).
 __Time:__ Took 5 days, averaging 4 hours every day 
 __Pros:__ Free. Really good theoretical background for Bayesian filtering and detailed mathematical derrivation of linear, extended, uncented and cubature Kalman filters together with particle filters. The course asks you do complete a few exercises after each lecture to deepen the new knowledge.
 __Cons:__ Most useful project-like exercises, for example MATLAB coding of filters is hidden behind approx 300 USD paycheck. 
-2) [filterpy](https://filterpy.readthedocs.io/en/latest/#id6) and [companion book](https://nbviewer.jupyter.org/github/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/table_of_contents.ipynb)
+
+2) Python library for Kalman and Particle filter - [filterpy](https://filterpy.readthedocs.io/en/latest/#id6) and [the companion book](https://nbviewer.jupyter.org/github/rlabbe/Kalman-and-Bayesian-Filters-in-Python/blob/master/table_of_contents.ipynb)
 __Time:__ Took around 10 days 6-8 hours a day
-__Pros:__
-__Cons:__
+__Pros:__ Companion book nicely builds explanations of Kalman filters from ground up. Self contained does not require any background knowledge on the topic, all necessary mathematics is presented. Author goes for an thorough but intuitive explanations before most of the math is presented. Jupyter notebook allows to create interactive examples where user is in control of the input and can study the consequences which add up to learning experience. Author also gives good examples not only for the application of filterpy but also design of Kalman filter for different cases.
+__Cons:__ haven't found any, beginner book totally worth every sencond spent
+
+3) edX course [Multi-Object Tracking for Autonomous Systems](https://courses.edx.org/courses/course-v1:ChalmersX+ChM013x+1T2020/course/)
+__Time__: SOT part 2 weeks,
+__Pros__: Very detailed, this time coding assignments are not locked behind the payceck
+__Cons__: Although being an awesome intro to object tracking, reqires very solid background builds on top sensor fusion course and additional knowledge statistics.
 
 # TODO
 0) Create Pipenv
---
+++ py/sot/sot.py
@@ -0,0 +1,135 @@
+import numpy as np
+from filterpy.kalman import predict, update
+from scipy.stats import multivariate_normal
+from collections import namedtuple
+
+class GaussianMixture:
+    def __init__(self, xs, Ps, ws):
+        self.xs = xs
+        self.Ps = Ps
+        self.ws = ws
+        self.count = len(self.xs)
+
+    def get_mixture(self, xval):
+        mixture = np.zeros(xval.size)
+        for i in range(self.count):
+            mixture += self.get_component(i, xval)
+        return mixture
+
+    def get_components(self, xval):
+        components = np.zeros((xval.size, self.count))
+        for i in range(self.count):
+            components[:,i] = self.get_component(i, xval)
+        return components
+
+    def get_component(self, i, xval):
+        return self.ws[i] * multivariate_normal(self.xs[i], self.Ps[i]).pdf(xval)
+
+    def get_pruned(self):
+        i = np.argmax(self.ws)
+        return self.xs[i], self.Ps[i]
+
+    def get_merged(self):
+        xm = np.tensordot(self.ws, np.array(self.xs), 1)
+        Pm = np.zeros(self.Ps[0].shape)
+        for x, P, w in zip(self.xs, self.Ps, self.ws):
+            Pm += w * (P + np.dot(x - xm, x - xm)) 
+        return xm, Pm
+
+def approximate_nn(xs, Ps, ws):
+    pass
+
+def approximate_pda(xs, Ps, ws):
+    pass
+
+def add_mixture(xs, Ps, ws):
+    pass
+
+def predict_measurement(x, P, z, R, H):
+    x_pred, P_pred = predict(x, P, H, R)
+    return multivariate_normal(x_pred, P_pred).pdf(z)
+
+def predict_mixture(xs, Ps, F, Q):
+    '''
+    xs - list of state
+    Ps - list of state covariance matrices
+    F - transition matrix 
+    Q - process noise matrix
+    '''
+    xs_pred, Ps_pred = [], []
+    for x, P in zip(xs, Ps):
+        x_pred, P_pred = predict(x, P, F, Q)
+        xs_pred.append(x_pred)
+        Ps_pred.append(P_pred)
+    return xs_pred, Ps_pred
+
+def update_mixture(xs, Ps, ws, z, R, H, Pd, lamc):
+    '''
+    xs - list of state
+    Ps - list of state covariance matrices
+    ws - list of weights
+    z - list of measurments 
+    R - measurement noise matrix
+    H - measurement matrix 
+    Pd - probability of detection
+    lamc - clutter intensity function handle
+    '''
+    hypotheses = range(0, len(z) + 1)
+    ws_u, xs_u, Ps_u = [],[],[]
+    for x_prev, P_prev, w_prev in zip(xs, Ps, ws):
+        for h in hypotheses:
+            if h == 0:
+                weight = w_prev * (1 - Pd)
+                ws_u.append(weight)
+                xs_u.append(x_prev)
+                Ps_u.append(P_prev)
+            else:
+                zt = z[h - 1]
+                weight = w_prev * Pd * predict_measurement(x_prev, P_prev, zt, R, H) / lamc(zt)
+                x_u, P_u = update(x_prev, P_prev, zt, R, H)
+                ws_u.append(weight)
+                xs_u.append(x_u)
+                Ps_u.append(P_u)
+
+    # normalize weights
+    ws_u = ws_u / np.sum(ws_u)
+    return xs_u, Ps_u, ws_u
+
+def run_example():
+    Z = [
+        [-1.3, 1.7],
+        [1.3],
+        [-0.3, 2.3],
+        [-2, 3],
+        [2.8],
+        [-3.5, 2.8]
+    ]
+
+    lamc = lambda c: 0.4 if np.abs(c) < 4 else 0
+
+    R = np.array([0.2])
+    H = np.array([1])
+    Q = np.array([0.35])
+    F = np.array([1])
+    PD = 0.9
+
+    priors = [GaussianMixture(
+        [np.array([0.5])],
+        [np.array([0.5])],
+        [np.array(1.0)])]
+    posteriors = []
+
+    # CALCULATE RECURSION
+    for k, z in enumerate(Z):
+        # update
+        xs_u, Ps_u, ws_u = update_mixture(priors[k].xs, priors[k].Ps, priors[k].ws, z, R, H, PD, lamc)
+        mixture = GaussianMixture(xs_u, Ps_u, ws_u)
+        posteriors.append(mixture)
+        # approximation
+        xp, Pp = mixture.get_merged()
+        # prediction
+        xs_p, Ps_p = predict_mixture(xs_u, Ps_u, F, Q)
+        priors.append(GaussianMixture(xs_p, Ps_p, ws_u))
+
+if __name__ == "__main__":
+    run_example()
